adc1e2bd60f03d39f56ec78b9c1822dd66b44b3e
alishkb <ashokouhabdi@gmail.com>
2020-12-23 14:07:01 +0330

    add database and change routing names

--- Store/Store/main.py
+++ Store/Store/main.py
@@ -1,24 +1,30 @@
 from flask import Blueprint, request, render_template, session, url_for, flash, g
 from werkzeug.utils import redirect
+from Store.db import db
 
 bp = Blueprint("main", __name__)
+
 
 @bp.route("/")
 def index():
     return render_template("index.html")
 
+
 @bp.route("/basket/")
 def basket():
     return render_template("basket.html")
+
 
 @bp.route("/category/<name>")
 def category(name):
     return render_template("category.html")
 
+
 @bp.route("/stuff/<name>/")
 def stuff(name):
-    return render_template("stuff.html") # <<<<<<<<<<<<<<<<<<<<
+    return render_template("stuff.html")  # <<<<<<<<<<<<<<<<<<<<
 
-@bp.route("/record/")
-def record():
-    return render_template("record.html")
+
+@bp.route("/basket/approve/")
+def approve():
+    return render_template("approve.html")
