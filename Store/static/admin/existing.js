3ab51a5a7c37ef677888b46e771fc9c09d8b8d28
mmshojaefar <mmshojaefar@aut.ac.ir>
2020-12-25 04:06:37 +0330

    solve a small bug

--- Store/static/admin/existing.js
+++ Store/static/admin/existing.js
@@ -1,4 +1,8 @@
 var url_list = 'http://127.0.0.1:5000/api/existing/list';
+var url_edit = 'http://127.0.0.1:5000/api/existing/edit/';
+var url_delete = 'http://127.0.0.1:5000/api/existing/delete/';
+
+
 $.get(url_list, function(response, status){
     var $tbody = $("tbody")
     JSON.parse(response).forEach(function(product){
@@ -21,15 +25,12 @@ $.get(url_list, function(response, status){
 })
 
 
-var url_edit = 'http://127.0.0.1:5000/api/existing/edit/';
-var url_delete = 'http://127.0.0.1:5000/api/existing/delete/';
-
 $(function(){
     $('tbody tr td:last-child button:first-child').click(function(){
         console.log(this);
         $data = $(this).closest('tr').find('td');
-        var name = $($data[0]).text(); 
-        var storehouse = $($data[1]).text(); 
+        var name = $($data[1]).text(); 
+        var storehouse = $($data[0]).text(); 
         $.post(url_edit, {
                 storehouse : storehouse,
                 name : name,
@@ -45,8 +46,8 @@ $(function(){
     $('tbody tr td:last-child button:last-child').click(function(){
         console.log(this);
         $data = $(this).closest('tr').find('td');
-        var name = $($data[0]).text(); 
-        var storehouse = $($data[1]).text(); 
+        var name = $($data[1]).text(); 
+        var storehouse = $($data[0]).text(); 
         console.log(name, storehouse);
         $.ajax({
             url : url_delete,
@@ -64,22 +65,3 @@ $(function(){
         });
     })
 })
-
-
-// var url_edit = 'http://127.0.0.1:5000/api/existing/delete";
-
-
-// سلام. یکی از دوستان سوال پرسید که توی flask وقتی با Get request پارامتر میفرستم مثل
-// /category/؟name=<category_name>
-// توی view چه طوری میگیریم؟
-
-// جواب اینه که مثلا تو مثال بالا با
-// request.args.get('name')
-// پارامتر name رو میگیریم.
-
-// میتونید از variable rule هم استفاده کنید:
-// @app.route('/category/<name>')
-// def category (name):
-//         ...
-// اون وقت url‌تون میشه:
-// /category/<category_name></category_name>
