c6339707dc3405df63c3f7dd1f0209a0954c8135
mmshojaefar <mmshojaefar@aut.ac.ir>
2020-12-25 03:54:58 +0330

    existing and price api, updated! now we can edit, delete and show the list by ajax. this is demo and i will update it soon

--- Store/api.py
+++ Store/api.py
@@ -5,17 +5,38 @@ from bson.json_util import dumps
 
 bp = Blueprint("api", __name__, url_prefix="/api")
 
-@bp.route("/product/list")
+
+@bp.route("/product/list/")
 def product_list():
     all_products = db.product.find({}, 
                     {'image':1, 'name':1, 'category':1, '_id':0})
     json_string = dumps(all_products)
     return json_string
 
-@bp.route("/existing/list")
+
+@bp.route("/existing/list/")
 def existing_list():
     all_products = db.product.find({}, 
                     {'storehouse':1 ,'name':1 ,'count':1, 'price':1, '_id':0})
     json_string = dumps(all_products)
     return json_string
 
+
+@bp.route("/existing/edit/", methods=['POST'])
+def existing_edit():
+    if request.method == 'POST':
+        storehouse = request.form['storehouse']
+        name = request.form['name']
+        price = request.form['price']
+        count = request.form['count']
+    print(storehouse, name, price, count)
+    return {}
+
+
+# @bp.route("/existing/delete/?name=<product_name>&storehouse=<store_name>", methods=['GET'])
+@bp.route("/existing/delete/", methods=['GET'])
+def existing_delete():
+    name = request.args.get('name')
+    storehouse = request.args.get('storehouse')
+    print(name, storehouse)
+    return {}
--- Store/static/admin/existing.js
+++ Store/static/admin/existing.js
@@ -1,5 +1,5 @@
-var url = 'http://127.0.0.1:5000/api/existing/list';
-$.get(url, function(response, status){
+var url_list = 'http://127.0.0.1:5000/api/existing/list';
+$.get(url_list, function(response, status){
     var $tbody = $("tbody")
     JSON.parse(response).forEach(function(product){
         var storehouse = product.storehouse;
@@ -11,9 +11,75 @@ $.get(url, function(response, status){
         row += "<td>" + name + "</td>";
         row += "<td>" + price + "</td>";
         row += "<td>" + count + "</td>";
-        row += "<td>" + '<a href=""> ویرایش</a><a href=""> حذف</a>' + "</td>";
+        row += "<td>" ;
+        row += "<button class='edit'> ویرایش</button>";
+        row += "<button class='delete'> حذف</button>";
+        row += "</td>";
         row += "</tr>";
         $tbody.append(row);
-    })
-    
+    })  
 })
+
+
+var url_edit = 'http://127.0.0.1:5000/api/existing/edit/';
+var url_delete = 'http://127.0.0.1:5000/api/existing/delete/';
+
+$(function(){
+    $('tbody tr td:last-child button:first-child').click(function(){
+        console.log(this);
+        $data = $(this).closest('tr').find('td');
+        var name = $($data[0]).text(); 
+        var storehouse = $($data[1]).text(); 
+        $.post(url_edit, {
+                storehouse : storehouse,
+                name : name,
+                price : 8500,
+                count : 30
+        },function(response, status){
+            if(status=='success')
+                console.log(status);
+            console.log(response);
+        })
+    })
+
+    $('tbody tr td:last-child button:last-child').click(function(){
+        console.log(this);
+        $data = $(this).closest('tr').find('td');
+        var name = $($data[0]).text(); 
+        var storehouse = $($data[1]).text(); 
+        console.log(name, storehouse);
+        $.ajax({
+            url : url_delete,
+            data : {
+                'name' : name,
+                'storehouse' : storehouse,
+            },
+            type : "GET",
+            success : function(){
+                console.log(5555);
+            },
+            error : function(){
+                console.log('eeee');
+            }
+        });
+    })
+})
+
+
+// var url_edit = 'http://127.0.0.1:5000/api/existing/delete";
+
+
+// سلام. یکی از دوستان سوال پرسید که توی flask وقتی با Get request پارامتر میفرستم مثل
+// /category/؟name=<category_name>
+// توی view چه طوری میگیریم؟
+
+// جواب اینه که مثلا تو مثال بالا با
+// request.args.get('name')
+// پارامتر name رو میگیریم.
+
+// میتونید از variable rule هم استفاده کنید:
+// @app.route('/category/<name>')
+// def category (name):
+//         ...
+// اون وقت url‌تون میشه:
+// /category/<category_name></category_name>
